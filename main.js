01a1ddf174d4613c6fd828ba921c444c4a0bef37
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-22 19:04:28 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+//Pretend there is a <p> with class "example"
+const myParagraph = document.querySelector('.example');
+//You can do many this with is
+myParagraph.textContent = 'This is my new text';
